78bcb72b8ca42a5c4504841e7108e5ad48170ef7
Artem <overin1@mail.ru>
2022-05-10 16:57:00 +0300

    fix alter

--- erp-core/src/main/resources/db/src/sql/000_05_modify_payment_status.sql
+++ erp-core/src/main/resources/db/src/sql/000_05_modify_payment_status.sql
@@ -1,4 +1,9 @@
 alter table ks
-    alter column payment_status type bool using payment_status::bool,
+    alter column payment_status set default null;
+
+alter table ks
+    alter column payment_status type bool using payment_status::bool;
+
+alter table ks
     alter column payment_status set default false;
 
